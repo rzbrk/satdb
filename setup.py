a54067ea28bf58769b9a33866ad11366d5e72f49
Jan Grosser <email@jan-grosser.de>
2021-05-15 19:28:47 +0200

    Added initial version of setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# install module/scripts with:
+# python setup.py install
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name = 'satdb',
+        version = '0.1',
+        author = 'Jan Grosser',
+        author_email = 'email@jan-grosser.de',
+#        license = '',
+#        description = '',
+#        long_description = open('README.md').read(),
+        packages = ['satdb'],
+        scripts = ['omm2db.py'],
+        install_requires = [
+            'mysql-connector-python',
+            'pyyaml'
+            ]
+        )
