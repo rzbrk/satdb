e09c04813e3cf2ac654c9d6e756989f4c41fec88
Jan Grosser <email@jan-grosser.de>
2021-06-23 23:34:13 +0200

    Cleaned up setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -3,14 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name = 'satdb',
-        version = '0.1',
-        author = 'Jan Grosser',
-        author_email = 'email@jan-grosser.de',
-#        license = '',
-#        description = '',
-#        long_description = open('README.md').read(),
         packages = find_packages(),
-#        scripts = ['omm2db.py', 'st_dl_latest.py', 'tle2db.py'],
         install_requires = [
             'mysql-connector-python',
             'pyyaml',
